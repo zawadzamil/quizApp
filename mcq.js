64829ad1c23f55a5ce7e3872f946e04f415bba87
zawadzamil <contact.zawadzamil@gmail.com>
2022-02-02 16:02:13 +0600

    3rd commit

--- mcq.js
+++ mcq.js
@@ -9,6 +9,11 @@ const removeButtonRadio = document.getElementById('removeRadio');
 const mainDiv = document.getElementById('optionDiv');
 const mainDivRadio = document.getElementById('optionDivRadio');
 const selectType = document.getElementById('type');
+const addQuestionMCQButton = document.getElementById('addQuestionMCQ');
+const addQuestionRadioButton = document.getElementById('addQuestionRadio');
+const addQuestionGapButton = document.getElementById('addQuestionGap');
+const addQuestionDescButton = document.getElementById('addQuestionDesc');
+
 
 const mcqDiv = document.getElementById('mcq');
 const radioDiv = document.getElementById('radio');
@@ -17,9 +22,12 @@ const descDiv = document.getElementById('desc');
 
 
 
-
+addQuestionMCQButton.style.display = 'none'
+addQuestionRadioButton.style.display = 'none'
+addQuestionGapButton.style.display = 'none'
+addQuestionDescButton.style.display = 'none'
 questionTypeDiv.style.display = 'none'
-addMoreButtoneMCQ.style.display = 'none'
+
 
 
 removeButton.style.display = 'none';
@@ -41,8 +49,8 @@ let j = 2;
 //Topic Select Event
 topic.addEventListener('change', function () {
     questionTypeDiv.style.display = 'block'
-   
-    
+
+
 
 })
 
@@ -50,8 +58,10 @@ topic.addEventListener('change', function () {
 //Question Type  Select Event
 
 selectType.addEventListener('change', function (event) {
-    
-        
+
+
+
+
 
 
     if (selectType.value.localeCompare('mcq') == 0) {
@@ -59,70 +69,89 @@ selectType.addEventListener('change', function (event) {
         radioDiv.style.display = 'none'
         gapDiv.style.display = 'none'
         descDiv.style.display = 'none'
-        
-        addMoreButtoneMCQ.style.display = 'block'
-        
-        console.log('i :' +i)
+
+        addQuestionMCQButton.style.display = 'inline-block'
+        addQuestionRadioButton.style.display = 'none'
+        addQuestionGapButton.style.display = 'none'
+        addQuestionDescButton.style.display = 'none'
+
+
+
+        console.log('i :' + i)
 
 
         if (i <= 2) {
             removeButton.style.display = 'none'
         }
-      
+
 
     }
-    if (selectType.value.localeCompare('radio') == 0)
-    {
+    if (selectType.value.localeCompare('radio') == 0) {
         radioDiv.style.display = 'block'
         mcqDiv.style.display = 'none'
         gapDiv.style.display = 'none'
         descDiv.style.display = 'none'
-        addMoreButtoneMCQ.style.display = 'none'
-        
-      
-       
-        console.log('j :' +j)
-        
 
-        
+        addQuestionMCQButton.style.display = 'none'
+        addQuestionRadioButton.style.display = 'inline-block'
+        addQuestionGapButton.style.display = 'none'
+        addQuestionDescButton.style.display = 'none'
+
+
+
+
+        console.log('j :' + j)
+
+
+
 
 
         if (j <= 2) {
             removeButtonRadio.style.display = 'none'
         }
     }
-    if (selectType.value.localeCompare('gap') == 0)
-    {
+    if (selectType.value.localeCompare('gap') == 0) {
         radioDiv.style.display = 'none'
         mcqDiv.style.display = 'none'
         gapDiv.style.display = 'block'
-        addMoreButtoneMCQ.style.display = 'none'
+
         descDiv.style.display = 'none'
-        
-      
-       
+
+        addQuestionMCQButton.style.display = 'none'
+        addQuestionRadioButton.style.display = 'none'
+        addQuestionGapButton.style.display = 'inline-block'
+        addQuestionDescButton.style.display = 'none'
+
+
+
     }
-    if (selectType.value.localeCompare('desc') == 0)
-    {
+    if (selectType.value.localeCompare('desc') == 0) {
         radioDiv.style.display = 'none'
         mcqDiv.style.display = 'none'
         gapDiv.style.display = 'none'
-        addMoreButtoneMCQ.style.display = 'none'
         descDiv.style.display = 'block'
+
+        addQuestionMCQButton.style.display = 'none'
+        addQuestionRadioButton.style.display = 'none'
+        addQuestionGapButton.style.display = 'none'
+        addQuestionDescButton.style.display = 'inline-block'
+
         
-      
-       
+     
+
+
+
     }
 
-   
 
 
-    
-       
-        
-       
 
-    
+
+
+
+
+
+
 
 
 });
@@ -131,13 +160,14 @@ selectType.addEventListener('change', function (event) {
 
 addButton.addEventListener('click', function (event) {
     i += 1;
-    console.log('i :' +i)
-   
+    console.log('i :' + i)
+
 
     const div = document.createElement('div');
     const externalHTML = `<label for="option2" >` + i + `</label>
                   <input type="text" name="option`+ i + `" id="" placeholder="Enter Option"
-                      class="p-2 px-4 ml-2  lg:w-2/6 w-4/6 bg-green-200 shadow-xl rounded-lg">`;
+                      class="p-2 px-4 ml-2  lg:w-2/6 w-4/6 bg-green-200 shadow-xl rounded-lg"> OR
+                      <input type="file" name="figure`+ i + `" id="figure" class="inline ml-4 relative rounded text-center w-full lg:w-4/12 p-4 sm:mt-4 ">`;
     div.innerHTML = externalHTML;
     div.classList.add('mb-2');
     div.classList.add('p-2');
@@ -154,7 +184,7 @@ removeButton.addEventListener('click', function () {
     if (i > 2) {
         mainDiv.removeChild(mainDiv.lastChild)
         i -= 1;
-        console.log('i :' +i)
+        console.log('i :' + i)
 
     }
     if (i <= 2) {
@@ -171,13 +201,14 @@ removeButton.addEventListener('click', function () {
 
 addButtonRadio.addEventListener('click', function (event) {
     j += 1;
-    console.log('j :' +j)
-    
+    console.log('j :' + j)
+
 
     const div = document.createElement('div');
     const externalHTML = `<label for="option2" >` + j + `</label>
                   <input type="text" name="option`+ j + `" id="" placeholder="Enter Option"
-                      class="p-2 px-4 ml-2  lg:w-2/6 w-4/6 bg-blue-300 shadow-xl rounded-lg">`;
+                      class="p-2 px-4 ml-2  lg:w-2/6 w-4/6 bg-blue-300 shadow-xl rounded-lg">
+                      OR <input type="file" name="figure`+ j + `" id="figure" class="inline ml-4  rounded text-center w-full lg:w-4/12 p-4 sm:mt-4 ">`;
     div.innerHTML = externalHTML;
     div.classList.add('mb-2');
     div.classList.add('p-2');
@@ -194,7 +225,7 @@ removeButtonRadio.addEventListener('click', function () {
     if (j > 2) {
         mainDivRadio.removeChild(mainDivRadio.lastChild)
         j -= 1;
-        console.log('j :' +j)
+        console.log('j :' + j)
 
     }
     if (j <= 2) {
@@ -225,51 +256,4 @@ removeButtonRadio.addEventListener('click', function () {
 
 //////////////////// END  Add more option or Remove option\\\\\\\\\\\\\\\\\\\
 
-//////////////// Add More Input MCQ\\\\\\\\\\\\\\\\\\\\\
-addMoreButtoneMCQ.addEventListener('click', function () {
-    const mcqParentDiv = document.getElementById('mcqParent')
-    const div = document.createElement('div');
-    const externalHTMl = ` <div class="mcq text-center mt-8 bg-blue-200 w-full rounded-lg shadow-xl " id="mcq">
-        <div class="question p-8 ">
-            Insert Your Question Bellow <br>
-            <textarea name="question" id="question" rows="1" class="bg-gray-200 lg:w-full p-4 mt-4 sm:w-full">
-
-                </textarea>
-            <div class="options h-full  " id="optionDiv">
-                <p>Select Options</p>
-
-
-                <div class="mb-2 p-2">
-
-                    <label for="option1">1</label>
-                    <input type="text" name="option1" id="" placeholder="Enter Option"
-                        class="p-2 px-4 ml-2 lg:w-2/6 w-4/6 bg-green-200 shadow-xl rounded-lg">
-
-                </div>
-                <div class="mb-2 p-2">
-
-                    <label for="option2">2</label>
-                    <input type="text" name="option2" id="" placeholder="Enter Option"
-                        class="p-2 px-4 ml-2 lg:w-2/6 w-4/6 bg-green-200 shadow-xl rounded-lg">
-
-                </div>
-
-
-            </div>
-            <button id="add" class="bg-gray-800 p-2 rounded-lg text-white hover:bg-green-900 text-sm lg:text-lg mb-2 lg:mb-0">Add More Option</button>
-            <button id="remove"  class="bg-red-800 p-2 rounded-lg text-white hover:bg-red-900 text-sm lg:text-lg">Remove Option</button>
-
-
-        </div>
-        
-       
-
-
-    </div>`
-    div.innerHTML = externalHTMl
-    div.style.verticalAlign = 'bottom'
-    mcqParentDiv.appendChild(div);
-
-
-})
 
